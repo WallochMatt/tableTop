a8854b5d39b3cff1806f921413369d56db100124
WallochMatt <matthewrwalloch@gmail.com>
2023-01-27 13:51:00 -0600

    feat: Created method for rolling a d20

--- Dice.cs
+++ Dice.cs
@@ -8,5 +8,18 @@ namespace tableTop
 {
     internal class Dice
     {
+        public static int timesToRoll;
+        public static Random roll;
+
+        static Dice() 
+        {
+            roll = new Random();
+        }
+
+        public static int Rolld20() 
+        {
+            int result = roll.Next(1, 21);
+            return result;
+        }
     }
 }
